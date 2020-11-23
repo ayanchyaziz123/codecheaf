99740c6bca1c391dcf75875cb43d964d870d6bab
Izhaan Ahmed Chy <aaziz9642@gmail.com>
2020-11-24 00:33:40 +0600

    add some solutions

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,8 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
 
-        ll min = INT_MAX;
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+        int count = 0;
+        int n1 = s1.size();
+        int n2 = s2.size();
 
-        for (ll i = 0; i < v2.size(); i++)
+        for (int i = 0; i < n2; i++)
         {
-
-            if (arr[v2[i]] > max)
+            for (int j = 0; j < n1; j++)
             {
+                if (s1[i] == s2[j])
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        cout << count << endl;
+    }
+
+    return 0;
+}
